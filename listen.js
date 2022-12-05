3ecd3d71797bb08f56041036fcbf6501248a260b
nasben64 <n_benashur@yahoo.com>
2022-12-05 11:59:21 +0000

    removed process.env.ENV from listen.js

--- listen.js
+++ listen.js
@@ -3,5 +3,5 @@ const app = require("./app");
 const { PORT = 8080 } = process.env;
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT} ${process.env.ENV}`);
+  console.log(`Listening on port ${PORT}`);
 });
